70f1268c3ba28e45819277f1dfe129c41578076a
Ziah <zvanblerck1@gmail.com>
2026-05-16 12:35:08 +0200

    added esc to exit game

--- Scenes/game_scene_default.gd
+++ Scenes/game_scene_default.gd
@@ -4,7 +4,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
-	#if Input.is_action_just_pressed()
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
