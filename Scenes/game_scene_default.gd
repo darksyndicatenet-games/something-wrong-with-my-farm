a4e5f060b204ea8bdc2f883c7e741bfbb5c9e8c4
Francis <194915602+francis-fredrickson@users.noreply.github.com>
2026-05-16 17:11:20 +0200

    HELP

--- Scenes/game_scene_default.gd
+++ Scenes/game_scene_default.gd
@@ -1,6 +1,21 @@
 extends Node2D
 
+@export var pens: Array[GamePen] = [$Pen_One, $Pen_Two, $Pen_Three]
 
+func try_to_place_item(new_item: Node2D) -> void:
+	var placed_successfully: bool = false
+	for pen in pens:
+		# Check if the current pen is empty
+		if not pen.is_occupied():
+			# Found one! Place the item and exit the loop immediately
+			pen.occupy_pen(new_item)
+			placed_successfully = true
+			break 
+			
+	# If we ran through the entire loop and never marked 'placed_successfully' as true
+	if not placed_successfully:
+		print("All pens are full!")
+		# Here you can also trigger a HUD message to show "All pens are full" to the player.
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,11 +1,18 @@
 extends CharacterBody2D
 
 @export var speed = 100
+@export var info_hud = MarginContainer
 
 var rotation_direction = 0
+var meat = 0
+var coins = 0
+var animals = 0
+var wheat = 0
 
 func _physics_process(_delta):
 	get_input()
+	if info_hud:
+		info_hud.update_display(meat, coins, animals, wheat)
 	
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
